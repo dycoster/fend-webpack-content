4621476c0800fe05868e5b84ae8aa1ed45600061
DYC <dy.coster@gmail.com>
2020-12-27 10:13:42 +0100

    finish lesson 2 setup

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+// CSS Loader look at later
+// import css from "../client/styles/file.css";
 
 console.log(checkForName);
 
